fa81c0b09e536d334780b42a201102369811bad5
devberkay <canbr29@gmail.com>
2023-03-03 13:04:17 +0000

    Mar 3, 2023, 3:04 PM

--- lib/view/home/widgets/action_button.dart
+++ lib/view/home/widgets/action_button.dart
@@ -10,8 +10,7 @@ class ActionButton extends HookConsumerWidget {
   const ActionButton({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isExpose =
-        useState(false); // true for expose , false for hide
+    final isExpose = useState(false); // true for expose , false for hide
     return LayoutBuilder(builder: (context, constraints) {
       return TextButton(
           onPressed: () {
@@ -19,22 +18,29 @@ class ActionButton extends HookConsumerWidget {
                 !ref.read(isExposeProvider.notifier).state;
           },
           style: ButtonStyle(
+              splashFactory: InkRipple.splashFactory,
               foregroundColor: MaterialStateProperty.resolveWith((states) {
                 if (states.contains(MaterialState.pressed) || isExpose.value) {
-                  return Colors.black;
+                  return Colors.red;
                 } else {
-                  return Colors.white;
+                  return Colors.green;
                 }
               }),
               shape: MaterialStateProperty.resolveWith((states) {
-                if (states.contains(MaterialState.pressed )|| isExpose.value) {
+                if (states.contains(MaterialState.pressed) || isExpose.value) {
                   return RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(constraints.maxHeight * 0.5),
                       side: BorderSide(
-                          color:  Colors.white,width: constraints.maxHeight*0.025));
+                          color: Colors.red,
+                          width: constraints.maxHeight * 0.02));
                 } else {
                   return RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(constraints.maxHeight * 0.5),
                       side: BorderSide(
-                          color: isExpose.value ? Colors.green : Colors.white));
+                          color: Colors.green,
+                          width: constraints.maxHeight * 0.02));
                 }
               }),
               backgroundColor: MaterialStateProperty.resolveWith((states) {
@@ -47,10 +53,8 @@ class ActionButton extends HookConsumerWidget {
               fixedSize: MaterialStatePropertyAll(
                   Size(constraints.maxWidth, constraints.maxHeight))),
           child: Text(
-            "Check Battery Level",
-            style: TextStyle(
-                fontSize: constraints.maxHeight * 0.25,
-                color: isExpose.value ? Colors.green : Colors.white),
+           isExpose ? "Check Battery Level" : "Stop Listening Battery Level",
+            style: TextStyle(fontSize: constraints.maxHeight * 0.25),
           ));
     });
   }
