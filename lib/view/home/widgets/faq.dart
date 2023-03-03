604a8b120219827f795363cc830f82881aaf0e43
devberkay <canbr29@gmail.com>
2023-03-03 20:28:24 +0000

    Mar 3, 2023, 10:28 PM

--- lib/view/home/widgets/faq.dart
+++ lib/view/home/widgets/faq.dart
@@ -1,5 +1,6 @@
 import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class Faq extends HookConsumerWidget {
@@ -23,18 +24,20 @@ class Faq extends HookConsumerWidget {
             alignment: Alignment.center),
         onPressed: () {
           Flushbar(
-            endOffset: Offset(dx, dy),
             message: "Developed by github.com/devberkay",
             messageSize: usableWidth * 0.035,
             padding: EdgeInsets.symmetric(horizontal: usableWidth * 0.025),
-            maxWidth: usableWidth * 0.9,
+            maxWidth: usableWidth * 0.85,
             borderRadius: BorderRadius.circular(usableHeight * 0.1),
             margin: EdgeInsets.only(
                 bottom: usableHeight * 0.025,
-                left: usableWidth % 0.05,
-                right: usableWidth * 0.05),
+                left: usableWidth % 0.075,
+                right: usableWidth * 0.075),
             mainButton: TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  context.pop();
+                },
+                style: TextButton.styleFrom(splashFactory: NoSplash.splashFactory),
                 child: Text("Dismiss",
                     style: TextStyle(
                         fontSize: usableWidth * 0.04,
