59520be311ffba073a470006babd65d66a471b5a
devberkay <canbr29@gmail.com>
2023-03-03 13:14:17 +0000

    Mar 3, 2023, 3:14 PM

--- lib/view/home/widgets/action_button.dart
+++ lib/view/home/widgets/action_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final isExposeProvider = StateProvider.autoDispose<bool>((ref) {
+final isMonitoringProvider = StateProvider.autoDispose<bool>((ref) {
   return false;
 });
 
@@ -10,25 +10,26 @@ class ActionButton extends HookConsumerWidget {
   const ActionButton({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isExpose =
-        ref.watch(isExposeProvider); // true for expose , false for hide
+    final isMonitoring =
+        ref.watch(isMonitoringProvider); // true for expose , false for hide
     return LayoutBuilder(builder: (context, constraints) {
       return TextButton(
           onPressed: () {
-            ref.read(isExposeProvider.notifier).state =
-                !ref.read(isExposeProvider.notifier).state;
+            ref.read(isMonitoringProvider.notifier).state =
+                !ref.read(isMonitoringProvider.notifier).state;
           },
           style: ButtonStyle(
               splashFactory: InkRipple.splashFactory,
+              overlayColor: MaterialStatePropertyAll(Colors.grey.shade400),
               foregroundColor: MaterialStateProperty.resolveWith((states) {
-                if (isExpose) {
+                if (isMonitoring) {
                   return Colors.red;
                 } else {
                   return Colors.green;
                 }
               }),
               shape: MaterialStateProperty.resolveWith((states) {
-                if (isExpose) {
+                if (isMonitoring) {
                   return RoundedRectangleBorder(
                       borderRadius:
                           BorderRadius.circular(constraints.maxHeight * 0.5),
@@ -45,7 +46,7 @@ class ActionButton extends HookConsumerWidget {
                 }
               }),
               backgroundColor: MaterialStateProperty.resolveWith((states) {
-                if (isExpose) {
+                if (isMonitoring) {
                   return Colors.white;
                 } else {
                   return Colors.white;
@@ -54,7 +55,7 @@ class ActionButton extends HookConsumerWidget {
               fixedSize: MaterialStatePropertyAll(
                   Size(constraints.maxWidth, constraints.maxHeight))),
           child: Text(
-            isExpose ? "Stop Listening" : "Check Battery Level",
+            isMonitoring ? "Stop Monitoring" : "Check Battery Level",
             textAlign: TextAlign.center,
             style: TextStyle(fontSize: constraints.maxHeight * 0.25),
           ));
