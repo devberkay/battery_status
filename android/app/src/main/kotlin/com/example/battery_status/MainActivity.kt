124303609dcf827a577eecca47711e5f7d65bdf9
devberkay <canbr29@gmail.com>
2023-03-04 08:52:50 +0000

    Mar 4, 2023, 10:52 AM

--- android/app/src/main/kotlin/com/example/battery_status/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/battery_status/MainActivity.kt
@@ -2,6 +2,13 @@ import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
 
 class MainActivity: FlutterActivity() {
   private val CHANNEL = "berkaycan.dev/battery"
@@ -14,4 +21,18 @@ class MainActivity: FlutterActivity() {
       // TODO
     }
   }
+
+    private fun getBatteryLevel(): Int {
+    val batteryLevel: Int
+    if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+      val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+      batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+    } else {
+      val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED)) 
+      batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+    }
+
+    return batteryLevel
+  }
+
 }
