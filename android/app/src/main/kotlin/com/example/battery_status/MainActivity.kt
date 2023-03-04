c4d5cd1af769cea2895b9146b3386009e725084f
devberkay <canbr29@gmail.com>
2023-03-04 11:07:38 +0000

    Mar 4, 2023, 1:07 PM

--- android/app/src/main/kotlin/com/example/battery_status/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/battery_status/MainActivity.kt
@@ -1,6 +1,41 @@
 package com.example.battery_status
 
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Bundle
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "berkaycan.dev/battery"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "getBatteryLevel") {
+                val batteryLevel = getBatteryLevel()
+                if (batteryLevel != -1) {
+                    result.success(batteryLevel)
+                } else {
+                    result.error("UNAVAILABLE", "Battery level not available.", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun getBatteryLevel(): Int {
+        val batteryStatus: Intent? = applicationContext.registerReceiver(null,
+            IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+        val level: Int = batteryStatus?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
+        val scale: Int = batteryStatus?.getIntExtra(BatteryManager.EXTRA_SCALE, -1) ?: -1
+        return (level / scale.toFloat() * 100).toInt()
+    }
 }
+
