21e9838c09cf0016cabf1b1ce0b60f664d1a88cc
devberkay <canbr29@gmail.com>
2023-03-04 09:04:50 +0000

    Mar 4, 2023, 11:04 AM

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
     let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-    let batteryChannel = FlutterMethodChannel(name: "samples.flutter.dev/battery",
+    let batteryChannel = FlutterMethodChannel(name: "berkaycan.dev/battery",
                                               binaryMessenger: controller.binaryMessenger)
     batteryChannel.setMethodCallHandler({
       (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
@@ -19,5 +19,18 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  private func receiveBatteryLevel(result: FlutterResult) {
+  let device = UIDevice.current
+  device.isBatteryMonitoringEnabled = true
+  if device.batteryState == UIDevice.BatteryState.unknown {
+    result(FlutterError(code: "UNAVAILABLE",
+                        message: "Battery level not available.",
+                        details: nil))
+  } else {
+    result(Int(device.batteryLevel * 100))
+  }
+  }
+
 }
 
