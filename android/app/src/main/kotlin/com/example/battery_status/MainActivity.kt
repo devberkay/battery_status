ac8806eb7da08ad6a3f1ffe02f8f68dab871b1d1
devberkay <canbr29@gmail.com>
2023-03-04 08:49:50 +0000

    Mar 4, 2023, 10:49 AM

--- android/app/src/main/kotlin/com/example/battery_status/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/battery_status/MainActivity.kt
@@ -1,6 +1,17 @@
-package com.example.battery_status
-
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+  private val CHANNEL = "berkaycan.dev/battery"
+
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+      call, result ->
+      // This method is invoked on the main thread.
+      // TODO
+    }
+  }
 }
